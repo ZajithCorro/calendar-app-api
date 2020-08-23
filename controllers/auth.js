fe4b67bb93443ec4008bc156e3e635ea7358b021
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-08-22 23:46:38 -0500

    All the logic to manage the routes from /api/auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,87 @@
-const createUser = (req, res) => {
-  const { name, email, password } = req.body;
+const bcrypt = require('bcryptjs');
 
-  res.status(201).json({ ok: true, msg: 'registro', name });
+const { hashPassword, comparePassword } = require('../helpers/bcrypt');
+const { generateToken } = require('../helpers/jwt');
+
+const User = require('../models/User');
+
+const createUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let usuario = await User.findOne({ email });
+
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Un usuario ya existe con ese correo',
+      });
+    }
+
+    usuario = new User(req.body);
+    usuario.password = hashPassword(password);
+
+    await usuario.save();
+
+    const token = await generateToken(usuario.id, usuario.name);
+
+    res
+      .status(201)
+      .json({ ok: true, uid: usuario.id, name: usuario.name, token });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrador de base de datos',
+    });
+  }
 };
 
-const loginUser = (req, res) => {
-  res.json({ ok: true, msg: 'login' });
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let usuario = await User.findOne({ email });
+
+    if (!usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'No existe un usuario registrado con ese correo',
+      });
+    }
+
+    const isValid = comparePassword(password, usuario.password);
+
+    if (!isValid) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Contraseña incorrecta',
+      });
+    }
+
+    const token = await generateToken(usuario.id, usuario.name);
+
+    res.json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrador de base de datos',
+    });
+  }
 };
 
-const renewToken = (req, res) => {
-  res.json({ ok: true, msg: 'renew' });
+const renewToken = async (req, res) => {
+  const token = await generateToken(req.uid, req.name);
+
+  res.json({ ok: true, token });
 };
 
 module.exports = {
