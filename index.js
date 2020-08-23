469e8bf865f4d332294919e9e8df97da46c96423
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-08-22 19:12:00 -0500

    Connection to mongo database

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 
+const dbConnection = require('./database/config');
+
 const app = express();
+dbConnection();
 
 app.use(express.static('public'));
 app.use(express.json());
