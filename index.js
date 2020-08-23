9e6f708b1721009d6661627b2ebcd3146261041f
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-08-22 23:39:52 -0500

    Morgan and cors added as middlewares

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const morgan = require('morgan');
+const cors = require('cors');
 
 const dbConnection = require('./database/config');
 
 const app = express();
 dbConnection();
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use('/api/auth', require('./routes/auth'));
 // TODO: CRUD: eventos
