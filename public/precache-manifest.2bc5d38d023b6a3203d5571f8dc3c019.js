2acc33ca2f73b55ac11e6238f621c6c56ea9bbdc
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-08-28 15:44:39 -0500

    Calendar app front-end

---
+++ public/precache-manifest.2bc5d38d023b6a3203d5571f8dc3c019.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db5807462749bb68229f1587afe5e99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b349d8de02ec761a6b31",
+    "url": "/static/css/2.a4083a40.chunk.css"
+  },
+  {
+    "revision": "c84701d9ca72938d638b",
+    "url": "/static/css/main.c1a5b9fa.chunk.css"
+  },
+  {
+    "revision": "b349d8de02ec761a6b31",
+    "url": "/static/js/2.074d20bd.chunk.js"
+  },
+  {
+    "revision": "7155aa3aaf2824ce502a909c4c5e2b94",
+    "url": "/static/js/2.074d20bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c84701d9ca72938d638b",
+    "url": "/static/js/main.353b0891.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
